fd5926443abd31f31fc282fff809bc575ea83ab6
roei910 <74923231+roei910@users.noreply.github.com>
2024-02-20 07:04:40 +0200

    fixing middleware

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,13 +1,15 @@
-const express = require("express"); //run local server
+const express = require("express");
 const cors = require("cors");
 
-module.exports = (req, res, next) => {
-    express.static("public");
-    express.urlencoded({ extended: true });
-    express.json();
-    cors({
-        origin: '*',
-        methods: ['GET', 'POST', 'DELETE', 'PATCH']
-    })
-    next();
-}
+const setupMiddlewares = (app) => {
+    app.use(express.static("public"));
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+    app.use(cors({
+        origin: "*",
+        methods: ["GET", "POST", "DELETE", "PATCH"],
+    }));
+    // app.use(customMiddleware);
+};
+
+module.exports = setupMiddlewares;
