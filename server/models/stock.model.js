8e4d5ce83dbac9948318978c28466ffbb2e2fd90
roei910 <74923231+roei910@users.noreply.github.com>
2024-02-20 07:06:20 +0200

    fetch stock
    the stocks are now being fetched from the mongo atlas db, if the stock is not existing it is being created from yahoo finance website

--- server/models/stock.model.js
+++ server/models/stock.model.js
@@ -3,7 +3,19 @@ const mongoose = require('mongoose');
 const stockSchema = new mongoose.Schema({
     name: String,
     symbol: String,
-    price: Number
+    stockPrice: Number,//at close, [data-field="regularMarketPrice"]
+    afterHoursPrice: Number,//after close, [data-field="postMarketPrice"]
+    oneYearEstimate: Number,//[data-test="ONE_YEAR_TARGET_PRICE-value"]
+    growthEstimate: {//https://finance.yahoo.com/quote/AAPL/analysis
+        //'[data-test="qsp-analyst"] > table:nth-child(7) > tbody' => the table
+        currentQtr: String,// > tr:nth-child(1) > td:nth-child(2)
+        nextQtr: String,// > tr:nth-child(2) > td:nth-child(2)
+        currentYear: String,// > tr:nth-child(3) > td:nth-child(2)
+        nextYear: String,// > tr:nth-child(4) > td:nth-child(2)
+        nextFiveYears: String,// > tr:nth-child(5) > td:nth-child(2)
+        pastFiveYears: String,// > tr:nth-child(6) > td:nth-child(2)
+    },
+    dateUpdated: Date
 });
 
 module.exports = mongoose.model('Stock', stockSchema);
--- server/routes/stocks.route.js
+++ server/routes/stocks.route.js
@@ -1,84 +1,130 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const authenticate = require("../config/authentication");
-
-const { JSDOM } = require('jsdom');
+const axios = require("axios");
+const { JSDOM } = require("jsdom");
 
 //importing db modules
 const Stock = require("../models/stock.model");
 
 //after authenctication
-router.get('/', authenticate, (req, res) => {
-    res.send('Product Home Page');
+router.get("/", authenticate, (req, res) => {
+  res.send("Product Home Page");
 });
 
-router.get("/symbol/:symbol", (req, res) => {
-    getStockPriceBySymbol("AAPL");
+router.get("/symbol/:symbol", async (req, res) => {
+  const stockSymbol = req.params.symbol;
 
-
-
-    const stockSymbol = req.params.symbol;
-    Stock.findOne({ symbol: stockSymbol }).then((found) => {
-      if (!found) {
-        let stock = getStockNameBySymbol(stockSymbol);
-        res.send(stock);
-        // getStockDataBySymbol(stockSymbol);
-        //create a new stock object and save to mongo db using mongoose
+  await Stock.findOne({ symbol: stockSymbol })
+    .then(async (stock) => {
+      if(!stock){
+        let stockInfo = await getStockFromYahoo(stockSymbol);
+        await Stock.create(stockInfo).then((stock) => console.log(`stock created successfully, ${stock.name}`));
+        res.status(201).json(stockInfo); //TODO: change to correct status, created new stock
       }
+      else
+        res.status(200).send({
+          message: "symbol was found in db",
+          data: stock});
+    })
+    .catch(() => {
+      res.status(400).send("couldnt find symbol");
     });
-    // res.json({ stock: stocks.find((stk) => stk.symbol === stockSymbol) });
-});
-  
-router.get("/name/:name", (req, res) => {
-    const stockName = req.params.name;
-  
-    const options = {
-      method: "GET",
-      url: "https://alpha-vantage.p.rapidapi.com/query",
-      params: {
-        keywords: stockName,
-        function: "SYMBOL_SEARCH",
-        datatype: "json",
-      },
-      headers: {
-        "X-RapidAPI-Key": "3cfd02f090msh4bd229c6a961da5p100578jsne25c8024ccee",
-        "X-RapidAPI-Host": "alpha-vantage.p.rapidapi.com",
-      },
-    };
-  
-    axios
-      .request(options)
-      .then((response) => {
-        res.status(200).json(response.data.bestMatches);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.sendStatus(500);
-      });
 });
 
-async function getStockPriceBySymbol(symbol) {
+router.get("/name/:name", (req, res) => {
+  const stockName = req.params.name;
+
+  const options = {
+    method: "GET",
+    url: "https://alpha-vantage.p.rapidapi.com/query",
+    params: {
+      keywords: stockName,
+      function: "SYMBOL_SEARCH",
+      datatype: "json",
+    },
+    headers: {
+      "X-RapidAPI-Key": "3cfd02f090msh4bd229c6a961da5p100578jsne25c8024ccee",
+      "X-RapidAPI-Host": "alpha-vantage.p.rapidapi.com",
+    },
+  };
+
+  axios
+    .request(options)
+    .then((response) => {
+      res.status(200).json(response.data.bestMatches);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
+
+async function getStockFromYahoo(symbol) {
+  try {
+    const stockPage = await fetch(`https://finance.yahoo.com/quote/${symbol}`);
+    const htmlStockPage = await stockPage.text();
+    const domStockPage = new JSDOM(htmlStockPage);
+    const stockPageDocument = domStockPage.window.document;
+
+    const analysisPage = await fetch(
+      `https://finance.yahoo.com/quote/${symbol}/analysis`
+    );
+    const analysisHtmlPage = await analysisPage.text();
+    const domAnalysisPage = new JSDOM(analysisHtmlPage);
+    const analysisPageDocument = domAnalysisPage.window.document;
+
+    const tableBodyElement = analysisPageDocument.querySelector(
+      '[data-test="qsp-analyst"] > table:nth-child(7) > tbody'
+    );
+
     try {
-        const response = await fetch('https://finance.yahoo.com/quote/' + symbol);
-        const html = await response.text();
-  
-        const dom = new JSDOM(html);
-        const doc = dom.window.document;
-        
-        // Find the element containing the stock price
-        const priceElement = doc.querySelector('[data-test="OPEN-value"]');
-        if (priceElement) {
-            const stockPrice = priceElement.textContent.trim();
-            console.log(`${symbol} stock price:`, stockPrice);
-            return stockPrice;
-        } else {
-            console.error('Failed to find AAPL stock price element');
-            return null;
-        }
-    } catch (error) {
-        console.error('Error fetching AAPL stock price:', error);
-        return null;
+      return {
+        name: stockPageDocument
+          .querySelector("#quote-header-info > div > div > div > h1")
+          .textContent.trim(),
+        symbol: symbol,
+        stockPrice: stockPageDocument
+          .querySelector(
+            '[data-test="qsp-price"][data-field="regularMarketPrice"]'
+          )
+          .textContent.trim(),
+        afterHoursPrice: stockPageDocument
+          .querySelector('[data-field="postMarketPrice"]')
+          .textContent.trim(),
+        oneYearEstimate: stockPageDocument
+          .querySelector('[data-test="ONE_YEAR_TARGET_PRICE-value"]')
+          .textContent.trim(),
+        growthEstimate: {
+          currentQtr: tableBodyElement
+            .querySelector("tr:nth-child(1) > td:nth-child(2)")
+            .textContent.trim(),
+          nextQtr: tableBodyElement
+            .querySelector("tr:nth-child(2) > td:nth-child(2)")
+            .textContent.trim(),
+          currentYear: tableBodyElement
+            .querySelector("tr:nth-child(3) > td:nth-child(2)")
+            .textContent.trim(),
+          nextYear: tableBodyElement
+            .querySelector("tr:nth-child(4) > td:nth-child(2)")
+            .textContent.trim(),
+          nextFiveYears: tableBodyElement
+            .querySelector("tr:nth-child(5) > td:nth-child(2)")
+            .textContent.trim(),
+          pastFiveYears: tableBodyElement
+            .querySelector("tr:nth-child(6) > td:nth-child(2)")
+            .textContent.trim(),
+        },
+        dateUpdated: new Date().toGMTString()
+      };
+    } catch (e) {
+      console.error("Failed to find stock information from yahoo finance");
+      return null;
     }
+  } catch (error) {
+    console.error("Error fetching AAPL stock price:", error);
+    return null;
+  }
 }
 
 async function getStockNameBySymbol(stockSymbol) {
@@ -96,19 +142,17 @@ async function getStockNameBySymbol(stockSymbol) {
     },
   };
 
-  axios
-    .request(options)
-    .then((response) => {
-      if(response.data.data.length > 0) {
-      console.log(response.data.data[0]);
-        return response.data.data[0];
-      } else {
-        return [];
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  try {
+    const response = await axios.request(options);
+    if (response.data.data.length > 0) {
+      return response.data.data[0];
+    } else {
+      return [];
+    }
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
 }
 
 async function getStockDataBySymbol(stockSymbol) {
@@ -121,14 +165,34 @@ async function getStockDataBySymbol(stockSymbol) {
     },
   };
 
-  axios
-    .request(options)
-    .then((response) => {
-      console.log(response.data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  try {
+    const response = await axios.request(options);
+    return response.data;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+}
+
+async function getInsightsBySymbol(stockSymbol) {
+  const axios = require("axios");
+
+  const options = {
+    method: "GET",
+    url: "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v3/get-insights",
+    params: { symbol: stockSymbol },
+    headers: {
+      "X-RapidAPI-Key": "3cfd02f090msh4bd229c6a961da5p100578jsne25c8024ccee",
+      "X-RapidAPI-Host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await axios.request(options);
+    return response.data.finance.result;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 module.exports = router;
