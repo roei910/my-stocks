295659c2d79a265cf78713a8be63def1f040d254
roei910 <roei910@gmail.com>
2024-05-04 20:55:47 +0300

    Update user-information.component.ts

--- client-angular/src/app/home/user-information/user-information.component.ts
+++ client-angular/src/app/home/user-information/user-information.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { UserService } from 'src/Services/user.service';
 
 @Component({
@@ -6,11 +6,41 @@ import { UserService } from 'src/Services/user.service';
   templateUrl: './user-information.component.html',
   styleUrls: ['./user-information.component.css']
 })
-export class UserInformationComponent {
+export class UserInformationComponent implements OnInit, AfterViewInit{
+  data: any;
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService) {
+    const tempData = [30, 25, 20, 15, 10];
 
-  GetUser(){
+    this.data = {
+      labels: ['AAPL', 'GOOGL', 'MSFT', 'AMZN', 'FB'],
+      datasets: [
+        {
+          // label: 'Sales',
+          data: tempData,
+          fill: true,
+          backgroundColor: this.getRandomColors(tempData.length),
+          borderColor: '#FFFFFF'
+        }
+      ]
+    };
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+  }
+
+  GetUser() {
     return this.userService.GetUser();
+  }
+
+  getRandomColors(count: number): string[] {
+    const colors: string[] = [];
+    for (let i = 0; i < count; i++) {
+    colors.push(`#${Math.floor(Math.random() * 16777215).toString(16)}`);
+    }
+    return colors;
   }
 }
