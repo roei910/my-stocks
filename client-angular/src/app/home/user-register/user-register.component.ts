7b2c7d39623a468437b36c49207d378219e15a1f
roei910 <roei910@gmail.com>
2024-05-05 15:26:53 +0300

    creating new user at button click
    adding a function to create the user, using axios creating a post request to the server and creating the user. when done redirecting to home.

--- client-angular/src/app/home/user-register/user-register.component.ts
+++ client-angular/src/app/home/user-register/user-register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { User } from '../../../Models/user';
+import axios from 'axios';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-register',
@@ -6,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-register.component.css']
 })
 export class UserRegisterComponent {
+  user?: User;
+  
+  constructor(private router: Router){ }
 
+  CreateUser(event: Event, name: string, email: string, password: string){
+    event?.preventDefault();
+
+    this.user = {
+      Name: name,
+      Password: password,
+      Email: email
+    };
+
+    axios
+    .post("https://localhost:7173/User", this.user)
+    .then(res => {
+      if(res.status == 201){
+        this.router.navigate(['/']);
+      }
+    });
+  }
 }
