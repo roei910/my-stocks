b6ccf3e9bec8ce9e94eb76df5d84d7cbe7effb8e
roei910 <roei910@gmail.com>
2024-05-18 22:29:24 +0300

    stock search component
    adding a button to navigate to the stock information

--- client-angular/src/app/home/stocks-search/stocks-search.component.ts
+++ client-angular/src/app/home/stocks-search/stocks-search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { DatabaseService } from 'src/Services/database.service';
 
 @Component({
@@ -7,11 +8,18 @@ import { DatabaseService } from 'src/Services/database.service';
   styleUrls: ['./stocks-search.component.css']
 })
 export class StocksSearchComponent {
+
   stocksList: any = null;
 
-  constructor(private database: DatabaseService) { }
+  constructor(private database: DatabaseService,
+    private router: Router
+  ) { }
 
   async SearchResults(searchTerm: string) {
     this.stocksList = await this.database.FindStocksBySearchTerm(searchTerm);
+  }
+
+  NavigateStockInformation(symbol: string) {
+    this.router.navigate(['stocks', 'information'], { queryParams: { stockSymbol: symbol }});
   }
 }
