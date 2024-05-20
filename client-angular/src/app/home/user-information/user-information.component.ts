a23740924cef375be8eb862d98a8202cda2d4b59
roei910 <roei910@gmail.com>
2024-05-20 22:38:55 +0300

    Update user-information.component.ts
    initializing the owned stocks to show a view of stocks in the users account, and the profit

--- client-angular/src/app/home/user-information/user-information.component.ts
+++ client-angular/src/app/home/user-information/user-information.component.ts
@@ -1,4 +1,8 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AnonymousSubject } from 'rxjs/internal/Subject';
+import { Share } from 'src/Models/share';
+import { Stock } from 'src/Models/stock';
+import { User } from 'src/Models/user';
 import { DatabaseService } from 'src/Services/database.service';
 import { UserService } from 'src/Services/user.service';
 
@@ -9,16 +13,34 @@ import { UserService } from 'src/Services/user.service';
 })
 export class UserInformationComponent{
   data: any;
-  user: any;
+  user?: User;
+  ownedStocks: {[stocksymbol: string] : Share} = {};
+  stocksDictionary!: {[stockSymbol: string]: Stock};
 
   constructor(private userService: UserService, private database: DatabaseService) {
-    let userEmail = this.userService.GetUser();
-    this.user = this.database.Users.find((user: any) => user.email == userEmail);
+    let userEmail = this.userService.GetUserEmail();
+    this.stocksDictionary = this.database.stocksDictionary;
 
-    const tempData = [30, 25, 20, 15, 10];
+    this.user = this.database.Users.find((user: User) => user.email == userEmail);
+
+    if(this.user == undefined)
+      return;
+
+    this.InitializeOwnedStocks();
+    // console.log(this.ownedStocks);
+    this.InitializeChartData();
+  }
+
+  InitializeChartData(){
+    const ownedStockSymbols = Object.keys(this.ownedStocks);
+    const tempData = ownedStockSymbols.map((symbol: string) => {
+      return this.ownedStocks[symbol].averagePrice * this.ownedStocks[symbol].amount;
+    })
+    // const tempData = [30, 25, 20, 15, 10];
 
     this.data = {
-      labels: ['AAPL', 'GOOGL', 'MSFT', 'AMZN', 'FB'],
+      // labels: ['AAPL', 'GOOGL', 'MSFT', 'AMZN', 'FB'],
+      labels: ownedStockSymbols,
       datasets: [
         {
           // label: 'Sales',
@@ -37,5 +59,51 @@ export class UserInformationComponent{
     colors.push(`#${Math.floor(Math.random() * 16777215).toString(16)}`);
     }
     return colors;
+  }
+
+  CountOwnedStocks(){
+    return Object.keys(this.ownedStocks).length;
+  }
+
+  GetTotalGain(){
+    let ownedStockSymbols = Object.keys(this.ownedStocks);
+    let totalGain = 0;
+
+    ownedStockSymbols.forEach((symbol: string) => {
+      let stockValue = this.stocksDictionary[symbol].price * this.ownedStocks[symbol].amount;
+      let purchaseValue = this.ownedStocks[symbol].averagePrice * this.ownedStocks[symbol].amount;
+      totalGain += stockValue - purchaseValue;
+    });
+
+    return totalGain;
+  }
+
+  InitializeOwnedStocks() {
+    Object.keys(this.user!.lists).forEach((listKey: string) => {
+      let stockSymbols = Object.keys(this.user!.lists[listKey]);
+  
+      stockSymbols.forEach((stockKey: string) => {
+        let shares = this.user!.lists[listKey][stockKey].shares;
+        let amount = 0;
+        let totalPrice = 0;
+  
+        shares.forEach((share: Share) => {
+          amount += share.amount;
+          totalPrice += share.amount * share.averagePrice;
+        });
+  
+        if(stockKey in this.ownedStocks){
+          let previousAmount = this.ownedStocks[stockKey].amount
+          amount += previousAmount;
+          totalPrice += this.ownedStocks[stockKey].averagePrice * previousAmount;
+        }
+  
+        if(amount > 0)
+          this.ownedStocks[stockKey] = {
+            amount: amount,
+            averagePrice: totalPrice / amount
+          };
+      })
+    });
   }
 }
