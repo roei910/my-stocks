bc7d51065a364e3943e35868ec07e9174d6cf9de
roei910 <roei910@gmail.com>
2024-05-29 07:52:57 +0300

    Update user.service.ts
    adding promise with boolean response to know if the user was created successfully or not

--- client-angular/src/Services/user.service.ts
+++ client-angular/src/Services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CookiesService } from './cookies.service';
 import { Router } from '@angular/router';
+import axios from 'axios';
 
 @Injectable({
   providedIn: 'root'
@@ -31,34 +32,37 @@ export class UserService {
   }
 
   async TryConnect(email: string, password: string): Promise<boolean> {
-    // var isConnected = await axios.get("https://localhost:7173/User?email=" + email)
-    //   .then(res => {
-    //     if (res.status == 200) {
-    //       this.cookieService.setCookie("email", res.data.email, 1);
-    //       this.router.navigate(['/']);
+    var isConnected = await axios.get("https://localhost:7173/User?email=" + email)
+      .then(res => {
+        if (res.status == 200) {
+          this.cookieService.setCookie("email", res.data.email, 1);
+          this.router.navigate(['/']);
 
-    //       return true;
-    //     }
+          return true;
+        }
 
-    //     return false;
-    //   });
-    var isConnected = email == "roei910@gmail.com";
-    if(isConnected){
-      this.cookieService.setCookie("email", "roei910@gmail.com", 1);
-      this.router.navigate(['/']);
-    }
+        return false;
+      });
+    // var isConnected = email == "roei910@gmail.com";
+    // if(isConnected){
+    //   this.cookieService.setCookie("email", "roei910@gmail.com", 1);
+    //   this.router.navigate(['/']);
+    // }
 
     return isConnected;
   }
 
-  async CreateUser(user: { Name: string; Password: string; Email: string; }) {
-    // await axios
-    // .post("https://localhost:7173/User", user)
-    // .then(res => {
-    //   if(res.status == 201){
-    //     this.router.navigate(['/']);
-    //   }
-    // });
-    this.router.navigate(['/']);
+  async CreateUser(user: { Name: string; Password: string; Email: string; }): Promise<boolean> {
+    var res = await axios
+      .post("https://localhost:7173/User", user)
+      .then(res => {
+        if(res.status == 201){
+          return true;
+        }
+
+        return false;
+      });
+
+    return res;
   }
 }
