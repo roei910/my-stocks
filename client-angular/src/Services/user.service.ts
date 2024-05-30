9789bde0818d9693ce2b084a9b318c1bbd719805
roei910 <roei910@gmail.com>
2024-05-30 07:07:14 +0300

    Update user.service.ts
    adding an option to make mock requests to a local list of users instead of to the server when needed. updating the functions to remove any routing from the user service

--- client-angular/src/Services/user.service.ts
+++ client-angular/src/Services/user.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { CookiesService } from './cookies.service';
-import { Router } from '@angular/router';
 import axios from 'axios';
+import { User } from 'src/Models/user';
 
+//TODO: create authentication service and move some functions from here to the auth service
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  
+  users: User[] = [];
+  isMock: boolean = false;
+
   constructor(private cookieService: CookiesService,
-    private router: Router
   ) { }
 
   GetUserEmail() {
@@ -32,27 +34,32 @@ export class UserService {
   }
 
   async TryConnect(email: string, password: string): Promise<boolean> {
+    if(this.isMock)
+      return this.TryConnectMock(email, password);
+
     var isConnected = await axios.get("https://localhost:7173/User?email=" + email)
       .then(res => {
         if (res.status == 200) {
           this.cookieService.setCookie("email", res.data.email, 1);
-          this.router.navigate(['/']);
 
           return true;
         }
 
         return false;
-      });
-    // var isConnected = email == "roei910@gmail.com";
-    // if(isConnected){
-    //   this.cookieService.setCookie("email", "roei910@gmail.com", 1);
-    //   this.router.navigate(['/']);
-    // }
+      })
+      .catch(err => {
+        console.log(err);
 
-    return isConnected;
+        return false;
+      });
+
+      return isConnected;
   }
 
-  async CreateUser(user: { Name: string; Password: string; Email: string; }): Promise<boolean> {
+  async CreateUser(user: User): Promise<boolean> {
+    if(this.isMock)
+      return this.CreateUserMock(user);
+
     var res = await axios
       .post("https://localhost:7173/User", user)
       .then(res => {
@@ -64,5 +71,31 @@ export class UserService {
       });
 
     return res;
+  }
+
+  CreateUserMock(user: User): boolean{
+    var foundUser = this.users.find((existingUser: User) => existingUser.email == user.email);
+
+    if(!foundUser)
+      this.users.push(user);
+    
+    return true;
+  }
+
+  TryConnectMock(email: string, password: string): boolean{
+    var foundUser = this.users.find((user: User) => user.email == email);
+
+    if(!foundUser)
+      return false;
+
+    var isUserAuthenticated = foundUser.password == password;
+    
+    if(isUserAuthenticated){
+      this.cookieService.setCookie("email", "roei910@gmail.com", 1);
+
+      return true;
+    }
+
+    return false;
   }
 }
