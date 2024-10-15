af266323b62d472229a7116764925153a10dc8a3
roei <roei910@gmail.com>
2024-10-15 10:08:01 +0300

    fixing error with component naming, updating server url in development and production environments

--- client-angular/src/environments/environment.production.ts
+++ client-angular/src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    server_url: "https://stocksapi-b6u3.onrender.com/api"
+    server_url: "https://splendid-sparkle-production.up.railway.app/api"
 }
