4b90b532dcc1f8ef2912aea6068d1414da4dff59
roei910 <74923231+roei910@users.noreply.github.com>
2024-10-25 17:43:30 +0300

    adding sorting by target mean price in stocks table

--- client-angular/src/app/partial-views/stocks-filter-pagination-table/stocks-filter-pagination-table.component.ts
+++ client-angular/src/app/partial-views/stocks-filter-pagination-table/stocks-filter-pagination-table.component.ts
@@ -14,6 +14,35 @@ export class StocksFilterPaginationTableComponent {
 
   ngOnInit(): void {
     this.stockService.GetAllStocks()
-      .subscribe(stocks => this.stocks = stocks);
+      .subscribe(stocks => {
+        this.stocks = stocks;
+        console.log(this.stocks);
+        
+      });
+  }
+
+  sortTargetPriceChangePercent(event: any) {
+    event.data.sort((a: Stock, b: Stock) => {
+      const valueA = this.targetPriceChangePercentage(a);
+      const valueB = this.targetPriceChangePercentage(b);
+      
+      const numA = valueA === "missing information" ? Number.NEGATIVE_INFINITY : parseFloat(valueA);
+      const numB = valueB === "missing information" ? Number.NEGATIVE_INFINITY : parseFloat(valueB);
+
+      let result = 0;
+      if (numA < numB) result = -1;
+      else if (numA > numB) result = 1;
+
+      return event.order === 1 ? result : -result;
+    });
+  }
+
+  targetPriceChangePercentage(stock: Stock){
+    if(stock.analysis?.targetMeanPrice == undefined || stock.analysis?.targetMeanPrice == 0)
+      return "missing information";
+
+    let targetChangePercentage = (stock?.analysis?.targetMeanPrice / stock.price * 100 - 100).toFixed(2);
+
+    return targetChangePercentage;
   }
 }
