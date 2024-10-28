aee4aad4c956f36727db114121fe6ed67ea7ccae
roei <roei910@gmail.com>
2024-10-28 08:07:25 +0200

    updating stock model from backend

--- client-angular/src/models/stocks/stock.ts
+++ client-angular/src/models/stocks/stock.ts
@@ -5,6 +5,11 @@ export class Stock {
     name!: string;
     symbol!: string;
     price!: number;
+    fiftyDayAverage!: number;
+    twoHundredDayAverage!: number
+    fiftyTwoWeekRange!: string;
+    fiftyTwoWeekLow!: number;
+    fiftyTwoWeekHigh!: number;
     analystRating?: string;
     updatedTime!: Date;
     analysis?: StockAnalysis;
